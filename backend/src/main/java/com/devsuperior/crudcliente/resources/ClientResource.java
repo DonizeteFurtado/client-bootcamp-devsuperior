74bb7796beb3409e981459f77d3e0ebd4e4e29e1
Donizete Furtado <fonizetefurtado24@gmail.com>
2021-05-16 23:37:02 -0500

    h2 database

--- backend/src/main/java/com/devsuperior/crudcliente/resources/ClientResource.java
+++ backend/src/main/java/com/devsuperior/crudcliente/resources/ClientResource.java
@@ -1,24 +1,26 @@
 package com.devsuperior.crudcliente.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.crudcliente.entities.Client;
+import com.devsuperior.crudcliente.services.ClientService;
 
 @RestController
 @RequestMapping(value = "/clients")
 public class ClientResource {
 	
+	@Autowired
+	private ClientService service;
+	
 	@GetMapping
 	public ResponseEntity<List<Client>> findAll(){
-		List<Client> list = new ArrayList<>();
-		list.add(new Client(1L, "Carlos", "052-065-089-98", 1546.52,null, 5));
-		list.add(new Client(1L, "Carlos", "052-065-089-98", 1546.52,null, 5));
+		List<Client> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO tb_client (name, cpf, income, birth_date,children) VALUES ('Carlos Braga','041.798.654-89',1254.84,NOW(),2);
+INSERT INTO tb_client (name, cpf, income, birth_date,children) VALUES ('Dunga Filho','987.325.985-12',54.84,NOW(),0);
+INSERT INTO tb_client (name, cpf, income, birth_date,children) VALUES ('Thamirys Fraga','126.478.965-32',981.84,NOW(),3);
+INSERT INTO tb_client (name, cpf, income, birth_date,children) VALUES ('Yago Victor','040.800.218-28',4.84,NOW(),1);
+INSERT INTO tb_client (name, cpf, income, birth_date,children) VALUES ('Julia Luna','833.455.285-84',454568.84,NOW(),10);
+INSERT INTO tb_client (name, cpf, income, birth_date,children) VALUES ('Flávia Alice','999.839.817-77',12484.84,NOW(),4);
